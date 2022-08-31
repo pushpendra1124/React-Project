d011f77ba298c3b9f23443a03d50bb609b60e373
Pushpendra Singh <99976461+govind124@users.noreply.github.com>
2022-08-31 09:30:05 +0530

    Add files via upload

---
+++ src/Component/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {Link} from "react-router-dom"
+
+
+ function ErrorPage() {
+  return (
+    <div>
+  <img className='error_Image' src="https://img.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg" alt="Not  Available" />
+    </div>
+  )
+}
+export default ErrorPage;
